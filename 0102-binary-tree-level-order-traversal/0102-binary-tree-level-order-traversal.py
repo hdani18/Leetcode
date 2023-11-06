0088653125db491bed24e9dad6fe0a28bee34d1b
hdani18 <93953695+hdani18@users.noreply.github.com>
2023-11-05 20:52:54 -0500

    Time: 11 ms (98.54%), Space: 14 MB (91.31%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,33 +1,41 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
         if root is None:
-            return []
-        q= [(root,0)]
-        dic ={}
+            return []  # Return an empty list if the root is None
 
+        q = [(root, 0)]
+        dic = {}
         while q:
-            crr,level = q.pop(0)
-
+            crr, level = q.pop(0)
             if level in dic:
                 dic[level].append(crr.val)
             else:
                 dic[level] = [crr.val]
-
             if crr.left is not None:
-                q.append((crr.left,level+1))
+                q.append((crr.left, level + 1))
             if crr.right is not None:
-                q.append((crr.right,level+1))
-
+                q.append((crr.right, level + 1))
         res = []
-        for value in dic.values():
-            res.append(value)
+        for j in dic.values():
+            res.append(j)
         return res
+
+
+
+
+
+
+
 
 
 
