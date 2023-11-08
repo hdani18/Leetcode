fe6c4e2123f5cc3814ee3f7ac437acece9cd1bbc
hdani18 <93953695+hdani18@users.noreply.github.com>
2023-11-08 11:41:37 -0500

    Time: 38 ms (51.47%), Space: 16 MB (94.86%) - LeetHub

--- 1283-reformat-date/1283-reformat-date.py
+++ 1283-reformat-date/1283-reformat-date.py
@@ -3,11 +3,8 @@ class Solution:
         val = date.split(" ")
         month_dic = {"Jan": "1" , "Feb":"2" , "Mar":"3", "Apr":"4", "May":"5", "Jun":"6", "Jul":"7", "Aug":"8", "Sep":"9", "Oct":"10", "Nov":"11", "Dec":"12"}
         
-        day = val[0][:-2] 
-        if len(day)==2:
-            day = val[0][:-2] 
-        else:
-            day = "0"+val[0][:-2]
+        day = val[0][:-2] if len(val[0][:-2]) == 2 else "0"+ val[0][:-2]
+        
         print(day)
         year = val[2]
         month = month_dic[val[1]]
