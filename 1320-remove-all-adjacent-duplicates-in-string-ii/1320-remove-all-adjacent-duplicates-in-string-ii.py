fba1b13588505840910c7410abf1655e5851a8ec
hdani18 <93953695+hdani18@users.noreply.github.com>
2023-11-10 11:31:11 -0500

    Time: 70 ms (81.79%), Space: 19.1 MB (65.97%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -10,12 +10,14 @@ class Solution(object):
         for i in s:
             if stack and stack[-1][0] ==i:
                 stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
             else:
                 stack.append([i,1])
 
-            if stack[-1][1] == k:
-                stack.pop()
-        print(stack)  
+            # if stack[-1][1] == k:
+            #     stack.pop()
+    
         ans = ""
         for i in stack:
             ans += (i[0]*i[1])  
