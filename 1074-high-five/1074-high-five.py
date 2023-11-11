3beaae5b9b60980449fc5198a47fc68f7162a5fa
hdani18 <93953695+hdani18@users.noreply.github.com>
2023-11-11 14:59:56 -0500

    Time: 63 ms (9.09%), Space: 13.6 MB (68.18%) - LeetHub

---
+++ 1074-high-five/1074-high-five.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def highFive(self, items):
+        """
+        :type items: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        dic = {}
+        for i, j in items:
+            if i in dic:
+                dic[i].append(j)
+            else:
+                dic[i] = [j]
+        print(dic)
+        ans = []
+        for key, value in dic.items():
+            value.sort(reverse=True)
+            print(value)
+            avg = int(round(sum(value[:5])/min(len(value),5)))
+            ans.append([key,avg])
+        return ans
+
+        
